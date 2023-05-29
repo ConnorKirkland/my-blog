f98344af347f9f2014409de32dc5bd45b163d68b
ConnorKirkland <connorwkirkland@gmail.com>
2023-05-29 10:47:52 -0400

    Rendering dummy post data

--- components/HomePage/FeaturedPosts.js
+++ components/HomePage/FeaturedPosts.js
@@ -1,7 +1,13 @@
-import React from "react";
+import styles from "./FeaturedPosts.module.css";
+import PostsGrid from "./posts/PostsGrid";
 
-const FeaturedPosts = () => {
-    return <div>FeaturedPosts</div>;
+const FeaturedPosts = (props) => {
+    return (
+        <section className={styles.latest}>
+            <h2>Featured Posts</h2>
+            <PostsGrid posts={props.posts} />
+        </section>
+    );
 };
 
 export default FeaturedPosts;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,46 @@
 import Hero from "../components/HomePage/Hero";
 import FeaturedPosts from "../components/HomePage/FeaturedPosts";
 
-const HomePage = () => {
+const DUMMY_POSTS = [
+    {
+        slug: "getting-started-with-nextjs",
+        title: "Getting Started with NextJS",
+        image: "getting-started-nextjs.png",
+        excerpt:
+            "NextJS is a the most popular framework for React! In this course, you'll learn NextJS from the ground up.",
+        date: "2021-02-10",
+    },
+    {
+        slug: "getting-started-with-nextjs2",
+        title: "Getting Started with NextJS",
+        image: "getting-started-nextjs.png",
+        excerpt:
+            "NextJS is a the most popular framework for React! In this course, you'll learn NextJS from the ground up.",
+        date: "2021-02-10",
+    },
+    {
+        slug: "getting-started-with-nextjs3",
+        title: "Getting Started with NextJS",
+        image: "getting-started-nextjs.png",
+        excerpt:
+            "NextJS is a the most popular framework for React! In this course, you'll learn NextJS from the ground up.",
+        date: "2021-02-10",
+    },
+    {
+        slug: "getting-started-with-nextjs4",
+        title: "Getting Started with NextJS",
+        image: "getting-started-nextjs.png",
+        excerpt:
+            "NextJS is a the most popular framework for React! In this course, you'll learn NextJS from the ground up.",
+        date: "2021-02-10",
+    },
+];
+
+const HomePage = (props) => {
     return (
         <>
             <Hero />
+            <FeaturedPosts posts={DUMMY_POSTS} />
         </>
     );
 };
